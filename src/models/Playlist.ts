ab5379e8e31efbe2af4eeb16d7901fbd6955a02e
Marco Cabrera <marco.cabrerahdez@gmail.com>
2022-05-12 12:02:17 +0100

    Creado models

---
+++ src/models/Playlist.ts
@@ -0,0 +1,61 @@
+import {Document, Schema, model} from 'mongoose';
+
+/**
+ * @interface PlaylistInterface Implements a playlist.
+ * @extends Document
+ */
+interface PlaylistInterface extends Document {
+  name: string,
+  songs: any[],
+  length: number,
+  genres: string[],
+}
+
+/**
+ * @const PlaylistSchema Playlist's schema
+ */
+const PlaylistSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: (value: string) => {
+      if (!value.match(/^[A-Z]/)) {
+        // eslint-disable-next-line max-len
+        throw new Error('El nombre de la playlist debe empezar en mayúscula');
+      }
+    },
+  },
+  songs: {
+    type: Object,
+    required: true,
+  },
+  length: {
+    type: Number,
+    required: true,
+    validate: (value: number) => {
+      if (value < 0) {
+        throw new Error('La duración no puede ser negativa');
+      }
+    },
+  },
+  genres: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: (value: string[]) => {
+      value.forEach((element) => {
+        if (!element.match(/^[A-Z]/)) {
+          // eslint-disable-next-line max-len
+          throw new Error('Los géneros de la canción deben empezar en mayúscula');
+        }
+      });
+    },
+  },
+});
+
+/**
+ * @const Playlist
+ */
+export const Playlist = model<PlaylistInterface>('Playlist', PlaylistSchema);
---
+++ src/models/Song.ts
@@ -0,0 +1,89 @@
+import {Document, Schema, model} from 'mongoose';
+
+/**
+ * @interface SongInterface Implements a song.
+ * @extends Document
+ */
+interface SongInterface extends Document {
+  name: string,
+  author: string,
+  length: number,
+  genres: string[],
+  single: boolean,
+  plays: number,
+}
+
+/**
+ * @const SongSchema Song's schema
+ */
+const SongSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: (value: string) => {
+      if (!value.match(/^[A-Z]/)) {
+        // eslint-disable-next-line max-len
+        throw new Error('Los nombres de las canciones deben empezar en mayúscula');
+      }
+    },
+  },
+  author: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: (value: string) => {
+      if (!value.match(/^[A-Z]/)) {
+        // eslint-disable-next-line max-len
+        throw new Error('Los nombres de los artistas deben empezar en mayúscula');
+      }
+    },
+  },
+  length: {
+    type: Number,
+    required: true,
+    validate: (value: number) => {
+      if (value < 0) {
+        throw new Error('La duración no puede ser negativa');
+      }
+    },
+  },
+  genres: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: (value: string[]) => {
+      value.forEach((element) => {
+        if (!element.match(/^[A-Z]/)) {
+          // eslint-disable-next-line max-len
+          throw new Error('Los géneros de la canción deben empezar en mayúscula');
+        }
+      });
+    },
+  },
+  single: {
+    type: Boolean,
+    required: true,
+    validate: (value: boolean) => {
+      if (typeof value !== 'boolean') {
+        throw new Error('Single es true or false');
+      }
+    },
+  },
+  plays: {
+    type: Number,
+    required: true,
+    validate: (value: number) => {
+      if (value < 0) {
+        throw new Error('Las reproducciones no pueden ser negativas');
+      }
+    },
+  },
+});
+
+/**
+ * @const Song
+ */
+export const Song = model<SongInterface>('Song', SongSchema);
