7d0315a45e1f03214e6320f4f54c702570442442
alu0101346908 <alu0101346908@ull.edu.es>
2022-05-15 11:51:50 +0100

    Heroku, mongoose, and index setup

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import './db/mongoose';
+import {getRouter} from './routers/get';
+import {defaultRouter} from './routers/default';
+import {join} from 'path';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(join(__dirname, '../public')));
+app.use(getRouter);
+app.use(defaultRouter);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
