3b7780e72012ef1a1febc1c861a3e0d2ee8a24df
alu0101346908 <alu0101346908@ull.edu.es>
2022-05-15 13:04:15 +0100

    Song delete endpoint fixed

--- src/routers/delete.ts
+++ src/routers/delete.ts
@@ -7,16 +7,37 @@ export const deleteRouter = express.Router();
 /**
  * Delete a specific song.
  */
-deleteRouter.delete('/song', (req, res) => {
+deleteRouter.delete('/song', async (req, res) => {
   if (!req.query.name) {
     res.status(400).send({
-      error: 'Name must be provided',
+      error: 'A name must be provided',
     });
-  } else {
-    Song.findOneAndDelete({name: req.query.name.toString()}).then((song) => {
-      if (!song) {
-        res.status(404).send();
-      }
-    });
+  }
+  try {
+    // eslint-disable-next-line max-len
+    const song = await Song.findOneAndDelete(req.query.name?{name: req.query.name.toString()}:{});
+    if (!song) {
+      return res.status(404).send();
+    }
+    return res.send(song);
+  } catch (error) {
+    return res.status(400).send();
+  }
+});
+
+/**
+ * Delete a song by id.
+ */
+deleteRouter.delete('/song/:id', async (req, res) => {
+  try {
+    const song = await Song.findByIdAndDelete(req.params.id);
+
+    if (!song) {
+      return res.status(404).send();
+    }
+
+    return res.send(song);
+  } catch (error) {
+    return res.status(400).send();
   }
 });
